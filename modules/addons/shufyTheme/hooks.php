66e6cf988b6dfade4ca5600a96f8042f70b00fa1
Ali <apna-github-email@gmail.com>
2026-07-29 15:44:58 +0500

    Pass shuffythemedirection and missing coodivoptions in hooks.php to render header and footer

--- modules/addons/shufyTheme/hooks.php
+++ modules/addons/shufyTheme/hooks.php
@@ -33,7 +33,10 @@ add_hook('ClientAreaPage', 1, function($vars) {
     $dbSettings = shufyTheme_get_all_settings_db();
 
     $defaults = [
-        'shuffythemeversion' => '1.3.2',
+        'shuffythemeversion'         => '1.3.2',
+        'shuffythemedirection'       => $template . '/includes/theme-core/header-layouts/header-default-layout.tpl',
+        'shuffythemedirectionfooter' => $template . '/includes/theme-core/footer-layouts/footer-default-layout.tpl',
+        
         'coodivsettings' => array_merge([
             'id'                   => '1',
             'customthemeloader'    => 'loaderdisbaled',
@@ -41,17 +44,55 @@ add_hook('ClientAreaPage', 1, function($vars) {
             'cartdropdown'         => 'activated',
             'notificationdropdown' => 'activated'
         ], $dbSettings),
+
         'coodivcolorsettings' => array_merge([
             'id'                => '1',
             'allowdarkmode'     => 'activated',
             'dafaultthemecolor' => 'default-color'
         ], $dbSettings),
+
         'coodivlayoutssettings' => array_merge([
             'id'                             => '1',
             'layoutsettingssidebarlayout'   => 'minimalist__sidebar',
             'layoutsettingssidebarposition' => 'sidebarpositionleft',
             'layoutsettingssidebarstyle'    => 'sidebarheaderlogo'
         ], $dbSettings),
+
+        'coodivsidebaroptions' => array_merge([
+            'id'                                      => '1',
+            'themesidebarsettingsfixedtopheader'     => 'activated',
+            'themesidebarsettingsfixedhorizontalmenu' => 'activated',
+            'themesidebarsettingsfixedsecondarymenu'  => 'activated',
+            'themesidebarsettingssidebaronhover'      => 'activated',
+            'themesidebarsettingschildonhover'        => 'activated',
+            'themesidebarsettingsdarkicons'           => 'activated',
+            'themesidebarsettingsallowusertoexpend'   => 'activated',
+            'themesidebarsettingsallowusertocollapse' => 'activated'
+        ], $dbSettings),
+
+        'coodivhomepagesettings' => array_merge([
+            'id'                                           => '1',
+            'themehomepagesettingmarketconnectbannaers'    => 'activated',
+            'themehomepagesettingmarketconnectbannaersnav' => 'activated',
+            'themehomepagesettinghomepagefeaturedsection'  => 'activated',
+            'themehomepagesettingservicesfeatures'        => 'activated',
+            'themehomepagesettingannouncements'            => 'activated',
+            'themehomepagesettingsavingbanner'             => 'activated',
+            'themehomepagesettingsubscribingsection'        => 'activated'
+        ], $dbSettings),
+
+        'coodivheaderoptions' => array_merge([
+            'id' => '1'
+        ], $dbSettings),
+
+        'coodivfooteroptions' => array_merge([
+            'id' => '1'
+        ], $dbSettings),
+
+        'coodivtypographieoptions' => array_merge([
+            'id' => '1'
+        ], $dbSettings),
+
         'CoodivMarketConnectServices' => []
     ];
 
